ac1da23e8012006c9b732396c5d92f8dbcb368ef
Surya JK <154452286+Surya-JK@users.noreply.github.com>
2024-04-01 08:37:34 +0530

    Add files via upload

---
+++ positive or negative cpp.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=12;
+	if(a>0)
+	{
+		cout<<a<<" is positive";
+	}
+	else if(a==0)
+	{
+		cout<<a<<" is zero";
+	}
+	else if(a<0)
+	{
+		cout<<a<<" is negative";
+	}
+	return 0;
+}
+
